3ac828b240a3dc7dd0d7b6df2d2ca3f3ff5e6467
Aya Adel <aya.adel.wd@gmail.com>
2021-05-08 16:04:04 +0200

    add splash screen

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,27 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "../Components/Header/index";
 import Search from "../Components/Search/Search";
 
-function App() {
+function Home() {
   const [displaySearch, setDisplaySearch] = useState<boolean>(false);
+  const [displayContent, setDisplayContent] = useState<boolean>(false);
+  const [displaySplash, setDisplaySplach] = useState<boolean>(true);
 
   const callbaskSearch = (callback: any) => {
     setDisplaySearch(callback);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setDisplaySplach(false);
+    }, 3000);
+
+    setTimeout(() => {
+      setDisplayContent(true);
+    }, 3000);
+  }, []);
+
   return (
     <div>
-      <Header handleSearch={callbaskSearch} />
-      <div>
-        <iframe
-          src="https://www.google.com/maps/d/embed?mid=17y--WIYiSD0jE3-HTtI8jQNtDz4&hl=en"
-          style={{ width: "100%", height: "1000px" }}
-        ></iframe>
-      </div>
-      {displaySearch && <Search />}
+      {displaySplash && (
+        <div className="image-vision-wrapper">
+          <div className="image-vision">
+            <img src="images/logo.png" alt="Logo" />
+          </div>
+        </div>
+      )}
+      {displayContent && (
+        <>
+          <Header handleSearch={callbaskSearch} />
+          <div>
+            <iframe
+              src="https://www.google.com/maps/d/embed?mid=17y--WIYiSD0jE3-HTtI8jQNtDz4&hl=en"
+              style={{ width: "100%", height: "1000px" }}
+            ></iframe>
+          </div>
+          {displaySearch && <Search />}
+        </>
+      )}
     </div>
   );
 }
 
-export default App;
+export default Home;
